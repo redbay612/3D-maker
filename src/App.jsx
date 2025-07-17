a479d1a815a65e501e7a960179852b95da72225f
bayliu <redkuegg@gmail.com>
2025-07-17 13:55:01 +0800

    修正：恢復三欄式佈局

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-// /src/App.jsx (最終穩定版)
+// /src/App.jsx (最終佈局修正版)
 
 import Scene from './components/Scene';
 import ControlPanel from './components/ControlPanel';
 import InfoPanel from './components/InfoPanel';
 
 function App() {
-  return (
-    <div className="w-screen h-screen bg-gray-900 relative">
-      {/* 恢復到最簡單的結構，確保所有元件都被渲染 */}
-      <ControlPanel />
-      <InfoPanel />
-      <div className="w-full h-full">
-        <Scene />
-      </div>
-    </div>
-  )
+    return (
+        // 使用一個 flexbox 容器來安排整體佈局
+        <div className="w-screen h-screen bg-gray-900 flex justify-between items-start p-4 gap-4 relative">
+
+            {/* 左側面板 */}
+            {/* z-10 確保它在 3D 場景之上 */}
+            <div className="z-10">
+                <ControlPanel />
+            </div>
+
+            {/* 中間的 3D 場景 */}
+            {/* 讓它佔滿剩餘的空間，並使用絕對定位填滿整個背景 */}
+            <div className="absolute inset-0 w-full h-full">
+                <Scene />
+            </div>
+
+            {/* 右側面板 */}
+            {/* z-10 確保它也在 3D 場景之上 */}
+            <div className="z-10">
+                <InfoPanel />
+            </div>
+
+        </div>
+    )
 }
 
 export default App;
