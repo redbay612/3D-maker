53da4f2c5f84e06ae65fc881e7d30460691f9825
bayliu <redkuegg@gmail.com>
2025-07-18 10:37:48 +0800

    功能升級：實現智慧排列與出界變色

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// /src/App.jsx (最終佈局修正版)
+
 import Scene from './components/Scene';
 import ControlPanel from './components/ControlPanel';
 import InfoPanel from './components/InfoPanel';
@@ -5,12 +7,17 @@ import InfoPanel from './components/InfoPanel';
 function App() {
     return (
         <div className="w-screen h-screen bg-gray-900 relative">
-            <ControlPanel />
-            <InfoPanel />
-            <div className="w-full h-full">
+            <div className="absolute inset-0 w-full h-full z-0">
                 <Scene />
             </div>
+            <div className="absolute top-4 left-4 z-10">
+                <ControlPanel />
+            </div>
+            <div className="absolute top-4 right-4 z-10">
+                <InfoPanel />
+            </div>
         </div>
-    );
+    )
 }
+
 export default App;
