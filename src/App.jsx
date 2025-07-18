7e5d4a2370606d387b0d1689d76789c0e6188398
bayliu <redkuegg@gmail.com>
2025-07-18 10:56:58 +0800

    回退到功能穩定的版本並修復渲染問題

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// /src/App.jsx (最終佈局修正版)
-
 import Scene from './components/Scene';
 import ControlPanel from './components/ControlPanel';
 import InfoPanel from './components/InfoPanel';
@@ -7,17 +5,12 @@ import InfoPanel from './components/InfoPanel';
 function App() {
     return (
         <div className="w-screen h-screen bg-gray-900 relative">
-            <div className="absolute inset-0 w-full h-full z-0">
+            <ControlPanel />
+            <InfoPanel />
+            <div className="w-full h-full">
                 <Scene />
             </div>
-            <div className="absolute top-4 left-4 z-10">
-                <ControlPanel />
-            </div>
-            <div className="absolute top-4 right-4 z-10">
-                <InfoPanel />
-            </div>
         </div>
-    )
+    );
 }
-
 export default App;
