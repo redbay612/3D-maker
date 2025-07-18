c5886ff0c612feeb65c36dfcfd7c775fb7cc13bf
bayliu <redkuegg@gmail.com>
2025-07-18 11:03:47 +0800

    最終修復：採用絕對定位並清理CSS以實現三欄式佈局

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import InfoPanel from './components/InfoPanel';
 
 function App() {
     return (
-        <div className="w-screen h-screen bg-gray-900 relative">
-            <ControlPanel />
-            <InfoPanel />
-            <div className="w-full h-full">
+        <div className="w-screen h-screen bg-gray-900 relative overflow-hidden">
+
+            <div className="absolute inset-0 w-full h-full z-0">
                 <Scene />
             </div>
+
+            <div className="absolute top-4 left-4 z-10">
+                <ControlPanel />
+            </div>
+
+            <div className="absolute top-4 right-4 z-10">
+                <InfoPanel />
+            </div>
+
         </div>
-    );
+    )
 }
+
 export default App;
